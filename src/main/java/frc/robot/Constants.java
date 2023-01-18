ebee9138b3e6a9efc2050bec7f93cf624bbac582
NOAH <smurfmuchies@gmail.com>
2023-01-18 16:26:11 -0600

    added compressor sub, and cpressor runs in init

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -13,7 +16,67 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
+    private Constants() { }
+    // Compressor
+    public static final PneumaticsModuleType PNEUMATICS_MODULE_TYPE = PneumaticsModuleType.CTREPCM;
+    public static final boolean COMPRESSOR_CLOSED_LOOP_CONTROL_ENABLED = true;
+    // Drivetrain
+    public static final int DRIVETRAIN_LEFT_ID1 = 0; //TODO: find values
+    public static final int DRIVETRAIN_LEFT_ID2 = 1;
+    public static final int DRIVETRAIN_RIGHT_ID1 = 2;
+    public static final int DRIVETRAIN_RIGHT_ID2 = 3;
+    
+
+    // Extreme
+    public static final int EXTREME_TRIGGER_PORT = 1;
+    public static final int EXTREME_SIDE_BUTTON_PORT = 2;
+    public static final int EXTREME_JOYSTICK_BOTTOM_LEFT_PORT = 3;
+    public static final int EXTREME_JOYSTICK_BOTTOM_RIGHT_PORT = 4;
+    public static final int EXTREME_JOYSTICK_TOP_LEFT_PORT = 5;
+    public static final int EXTREME_JOYSTICK_TOP_RIGHT_PORT = 6;
+    public static final int EXTREME_BASE_FRONT_LEFT_PORT = 7;
+    public static final int EXTREME_BASE_FRONT_RIGHT_PORT = 8;
+    public static final int EXTREME_BASE_MIDDLE_LEFT_PORT = 9;
+    public static final int EXTREME_BASE_MIDDLE_RIGHT_PORT = 10;
+    public static final int EXTREME_BASE_BACK_LEFT_PORT = 11;
+    public static final int EXTREME_BASE_BACK_RIGHT_PORT = 12;
+    public static final int EXTREME_STICK_X_AXIS_ID = 0;
+    public static final int EXTREME_STICK_Y_AXIS_ID = 1;
+    public static final int EXTREME_STICK_Z_AXIS_ID = 2;
+    public static final int EXTREME_SLIDER_AXIS_ID = 3;
+    public static final int EXTREME_POV_UP_DEGREES = 0;
+    public static final int EXTREME_POV_UP_RIGHT_DEGREES = 45;
+    public static final int EXTREME_POV_RIGHT_DEGREES = 90;
+    public static final int EXTREME_POV_DOWN_RIGHT_DEGREES = 135;
+    public static final int EXTREME_POV_DOWN_DEGREES = 180;
+    public static final int EXTREME_POV_DOWN_LEFT_DEGREES = 225;
+    public static final int EXTREME_POV_LEFT_DEGREES = 270;
+    public static final int EXTREME_POV_UP_LEFT_DEGREES = 315;
+
+    // Xbox Controller
+  public static final int XBOX_POV_UP_DEGREES = 0;
+  public static final int XBOX_POV_UP_RIGHT_DEGREES = 45;
+  public static final int XBOX_POV_RIGHT_DEGREES = 90;
+  public static final int XBOX_POV_DOWN_RIGHT_DEGREES = 135;
+  public static final int XBOX_POV_DOWN_DEGREES = 180;
+  public static final int XBOX_POV_DOWN_LEFT_DEGREES = 225;
+  public static final int XBOX_POV_LEFT_DEGREES = 270;
+  public static final int XBOX_POV_UP_LEFT_DEGREES = 315;
+  public static final int XBOX_POV_RELEASED_DEGREES = -1;
+  public static final RumbleType XBOX_LEFT_RUMBLE = RumbleType.kLeftRumble;
+  public static final RumbleType XBOX_RIGHT_RUMBLE = RumbleType.kRightRumble;
+  
+    // Button Box
+    public static final int BUTTON_BOX_TOP_WHITE_PORT = 2;
+    public static final int BUTTON_BOX_BIG_WHITE_PORT = 3;
+    public static final int BUTTON_BOX_MIDDLE_RED_PORT = 4;
+    public static final int BUTTON_BOX_BOTTOM_WHITE_PORT = 5;
+    public static final int BUTTON_BOX_TOP_RED_PORT = 6;
+    public static final int BUTTON_BOX_GREEN_PORT = 7;
+    public static final int BUTTON_BOX_MIDDLE_WHITE_PORT = 8;
+    public static final int BUTTON_BOX_BIG_RED_PORT = 14;
+    public static final int BUTTON_BOX_YELLOW_PORT = 15;
+    public static final int BUTTON_BOX_BOTTOM_RED_PORT = 16;
+  
+  
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,9 +4,11 @@
 
 package frc.robot;
 
-import frc.robot.Constants.OperatorConstants;
+import frc.robot.Constants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.CompressorSub;
+import frc.robot.subsystems.DriveSub;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -22,12 +24,21 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
+  private final DriveSub m_driveSub = new DriveSub();
+  private final CompressorSub m_CompressorSub = new CompressorSub();
+
+  
+
   private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  new CommandXboxController(0);
+
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+   m_CompressorSub.setClosedLoopControl(Constants.COMPRESSOR_CLOSED_LOOP_CONTROL_ENABLED);
+   //m_CompressorSub.enableDigital();
     // Configure the trigger bindings
     configureBindings();
   }
@@ -42,13 +53,10 @@ public class RobotContainer {
    * joysticks}.
    */
   private void configureBindings() {
-    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
-
-    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
-    // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+  
+    //m_driveSub.setDefaultCommand(m_driveSub.run(() -> m_driveSub.arcadeDrive(0, 0)));
+    m_driveSub.setDefaultCommand(m_driveSub.run(() -> m_driveSub.arcadeDrive(m_driverController.getLeftY(), m_driverController.getRightX())));
+    
   }
 
   /**
